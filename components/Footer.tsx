f568f15081ce5e5f757c65d54c431ef6c1dace03
Alex Thomas <48531362+AlexThomas-Graft0@users.noreply.github.com>
2026-07-04 15:42:19 +0100

    add components/Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,5 @@
+export const Footer = () => (
+  <footer className="bg-[#111827] text-white p-12 text-center">
+    <p>© 2023 tatŵ Studio. All Rights Reserved. Based in Cardiff, Wales.</p>
+  </footer>
+);
